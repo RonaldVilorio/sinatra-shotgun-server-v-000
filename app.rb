b98c194e18af6c3b3a10f83342b977747e9ff442
ronald vilorio <soccer23415@gmail.com>
2018-01-14 21:27:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require_relative 'config/environment'
-
-class App < Sinatra::Base
-
-  get '/' do 
-    "Welcome to your app!!!! "
-  end
-
-end
+# require_relative 'config/environment'
+#
+# class App < Sinatra::Base
+#
+#   get '/' do
+#     "Started my server using Shotgun "
+#   end
+#
+# end
